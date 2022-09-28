435f7c0a26780e5c94b7ca400ad7ee33241e5c27
Ashley Nguyen <ashley.nguyen@kpmg.fi>
2022-09-28 12:33:30 +0300

    Using lib zero_one_loss from sklearn.metrics

--- generalization_bound.py
+++ generalization_bound.py
@@ -1,6 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.datasets import make_moons
+from sklearn.metrics import zero_one_loss
 import random
 import math
 
@@ -32,9 +33,6 @@ train = order[:n] # take half of data to train
 Xtr = X[train, :] #coordinate of training data
 ytr = y[train] #train has 200 data so ytr will generate 200 random 0,1 values
 
-# Xtst = X[test, :]
-# ytst = y[test]
-
 # ========================================================================
 # classifier
 
@@ -44,8 +42,6 @@ from sklearn.linear_model import Perceptron as binary_classifier
 
 # # It can be used like this:
 bc = binary_classifier()
-# bc.fit(Xtr, ytr)  # train the classifier on training data
-# preds = bc.predict(Xtst)  # predict with test data
 
 # ========================================================================
 # setup for analysing the Rademacher complexity
@@ -54,15 +50,12 @@ bc = binary_classifier()
 print_at_n = [20, 50, 100]
 # when analysing Rademacher complexity, take always n first samples from training set, n as in this array
 
-#Classifier
-
 def cal_gen_bound(m):
     Xtraining = Xtr[:m,:] #the first m samples of the training set
     Ytraining = ytr[:m]
     Yrandom = np.zeros(m) #Defining an array for random labels
     
-   
-    emp_risk = cal_emp_risk(m,  Xtraining, Ytraining)
+    emp_risk = cal_emp_risk(Xtraining, Ytraining)
     rademacher_risk = cal_rademacher_risk(m, Xtraining, Yrandom)
     
     Term = 3 * math.sqrt((math.log(2/0.05))/(2*m))
@@ -70,38 +63,23 @@ def cal_gen_bound(m):
     return R
 
 
-def cal_emp_risk (m, Xtraining, Ytraining):
-    count = 0
+def cal_emp_risk (Xtraining, Ytraining):
     bc.fit(Xtraining, Ytraining) #Now with training samples, not random labels
     Ypred = bc.predict(Xtraining) #Seeing how good is the model
-    
-    for j in range(0, m):
-        if Ypred[j] != Ytraining[j]: #Seeing if these match or not and counting them
-            count += 1
-        emp_risk = count/m
-        # print("Count", j, count, m, emp_risk)
-    return emp_risk
+    return zero_one_loss(Ytraining, Ypred)
 
 def cal_rademacher_risk(m, Xtraining, Yrandom):
-    count = 0
-    Epsilon = np.zeros(200)#Defining an array for empirical risk
-    Total_epsilon = 0
-    for rango in range(0, 200): #To calculate the average risks, 200 samples of random sets
+    epsilon = np.zeros(200)#Defining an array for empirical risk
+
+    for element in range(0, 200): #To calculate the average risks, 200 samples of random sets
         for length in range(0,m):
             Yrandom[length] = random.randint(0, 1) #Preparing the random labels
         bc.fit(Xtraining, Yrandom) #Creating the model
         Ypred = bc.predict(Xtraining) #Seeing how well
         
-        for j in range(0, m): #Comparing both labels to count
-            if Ypred[j] != Yrandom[j]:
-                count += 1 #Counting
-        Epsilon[rango] = count/m #Calculating empirical risk for each M sample
-        count = 0
+        epsilon[element] = zero_one_loss(Yrandom, Ypred) #Calculating empirical risk for each M sample
         
-    for each in range(0, 200):
-        Total_epsilon += Epsilon[each]
-    
-    return (1/2) - Total_epsilon/200 #Second term of Rademacher
+    return (1/2) - np.sum(epsilon)/200 #Second term of Rademacher
 
 for i in print_at_n:
     print("Rademacher " + str(i) + " samples: " + str(cal_gen_bound(i)))
